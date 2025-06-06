9e926bf8e85c16ae19b1c169e303024850786dc4
IceZyzel <icezyzel@gmail.com>
2025-06-06 11:30:12 +0200

    deploy for http testing

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,17 +13,3 @@ locals {
   cluster_name = var.clusterName
 }
 
-module "alb_irsa" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "5.39.0"
-
-  role_name_prefix = "${local.cluster_name}-alb-controller-"
-  attach_load_balancer_controller_policy = true
-
-  oidc_providers = {
-    main = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
-    }
-  }
-}
